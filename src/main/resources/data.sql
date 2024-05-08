78a998a9d59d725c146b48f714b1b82721efd737
Revathi-Kamineni <104614509+Revathi-Kamineni@users.noreply.github.com>
2024-05-08 20:30:28 +0530

    Update data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,25 +1,24 @@
-drop table if exists message;
-drop table if exists account;
-create table account (
-    accountId int primary key auto_increment,
-    username varchar(255) not null unique,
-    password varchar(255)
+DROP TABLE IF EXISTS message;
+DROP TABLE IF EXISTS account;
+CREATE TABLE account (
+    account_id INT PRIMARY KEY AUTO_INCREMENT,
+    username VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255)
 );
-create table message (
-    messageId int primary key auto_increment,
-    postedBy int,
-    messageText varchar(255),
-    timePostedEpoch bigint,
-    foreign key (postedBy) references account(accountId)
+CREATE TABLE message (
+    message_id INT PRIMARY KEY AUTO_INCREMENT,
+    posted_by INT,
+    message_text VARCHAR(255),
+    time_posted_epoch BIGINT,
+    FOREIGN KEY (posted_by) REFERENCES  account(account_id)
 );
 
 -- Starting test values with ids of 9999 to avoid test issues
-insert into account values (9999, 'testuser1', 'password');
-insert into account values (9998, 'testuser2', 'password');
-insert into account values (9997, 'testuser3', 'password');
-insert into account values (9996, 'testuser4', 'password');
+INSERT INTO account VALUES (9999, 'testuser1', 'password');
+INSERT INTO account VALUES (9998, 'testuser2', 'password');
+INSERT INTO account VALUES (9997, 'testuser3', 'password');
+INSERT INTO account VALUES (9996, 'testuser4', 'password');
 
-insert into message values (9999, 9999,'test message 1',1669947792);
-insert into message values (9997, 9997,'test message 2',1669947792);
-insert into message values (9996, 9996,'test message 3',1669947792);
-
+INSERT INTO message VALUES (9999, 9999,'test message 1',1669947792);
+INSERT INTO message VALUES (9997, 9997,'test message 2',1669947792);
+INSERT INTO message VALUES (9996, 9996,'test message 3',1669947792);
